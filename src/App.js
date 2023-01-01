2c56232f56bdd13e7163ed891c6001ad65b368a9
Adam Glowacki <adam-glowacki@hotmail.com>
2023-01-01 23:59:55 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Central from './containers/Central.tsx';
+import "./style/app.css"
+
+function App() {
+  return (
+    <div className='app'>
+      <Central/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Central.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import "../style/central.css"
+
+const Central = props => {
+  return (
+    <div className='central'>
+      <div className='textey'>Some text here</div>
+    </div>
+  )
+}
+
+Central.propTypes = {}
+
+export default Central
